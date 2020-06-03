15a681d6969dee6d7ff2717aae354a92a74540c0
Evan Pratten <ewpratten@gmail.com>
2020-06-03 15:16:17 -0400

    Add put and get functions for UART

--- stdavr/uart/uart.cc
+++ stdavr/uart/uart.cc
@@ -1,3 +1,13 @@
+/**
+ * The following documents help a lot with porting my os328 UART code to 32u4
+ * boards:
+ * 
+ *  - https://appelsiini.net/2011/simple-usart-with-avr-libc/
+ *  - https://arduino.stackexchange.com/questions/19355/uart-error-can-not-compile-on-arduino-yun-atmega32u4
+ *  - https://cache.amobbs.com/bbs_upload782111/files_22/ourdev_508497.html
+ *  - https://www.avrfreaks.net/forum/fdevsetupstream-c
+ */
+
 #include <avr/io.h>
 
 #include <stdavr/uart/uart.hh>
@@ -77,12 +87,32 @@ void init(BaudRate rate) {
     }
 
     // Configure streams
-    streams::output.put = (int (*)(char, __file*))putc;
+    streams::output.put = (int (*)(char, __file *))putc;
     streams::output.get = nullptr;
     streams::output.flags = _FDEV_SETUP_WRITE;
     streams::input.put = nullptr;
     streams::input.get = getc;
     streams::input.flags = _FDEV_SETUP_READ;
+
+    // Overwrite default streams
+    stdout = &uart_output;
+    stdin = &uart_input;
+}
+
+void putc(char c, FILE *stream) {
+    if (c == '\n') {
+        uart_putchar('\r', stream);
+    }
+
+    // Wait for data
+    loop_until_bit_is_set(UCSR0A, UDRE0);
+    UDR0 = c;
+}
+
+void getc(FILE *stream __attribute__((unused))) {
+    // Wait for data
+    loop_until_bit_is_set(UCSR0A, RXC0);
+    return UDR0;
 }
 
 }  // namespace uart
